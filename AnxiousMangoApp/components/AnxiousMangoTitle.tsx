5c069d29d23e519315573bd782003b321a99e953
Monica Iqbal <monica.iqbal@mail.utoronto.ca>
2020-11-15 12:10:28 -0500

    added Image component in View component

--- AnxiousMangoApp/components/AnxiousMangoTitle.tsx
+++ AnxiousMangoApp/components/AnxiousMangoTitle.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 import {Image, StyleSheet, View} from "react-native";
 export default function AnxiousMangoTitle()  {
     return <>
-        <View style={styles.getStartedContainer}>Anxious Mango</View>
+        <View style={styles.getStartedContainer}>
+            Anxious Mango
+            <Image source={require('../mascot-1.png')}/>
+        </View>
         {/* TODO add the anxious mango image here and make sure it scales properly */}
-        <Image source={require('../mascot-1.png')}/>
     </>
 
 }
