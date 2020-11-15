2b95763cc9ca0dde1029c4c39e7c5acfb33ed7d2
Monica Iqbal <monica.iqbal@mail.utoronto.ca>
2020-11-15 11:54:48 -0500

    added correct path to logo in AnxiousMangoTitle

--- AnxiousMangoApp/components/AnxiousMangoTitle.tsx
+++ AnxiousMangoApp/components/AnxiousMangoTitle.tsx
@@ -4,7 +4,7 @@ export default function AnxiousMangoTitle()  {
     return <>
         <View style={styles.getStartedContainer}>Anxious Mango</View>
         {/* TODO add the anxious mango image here and make sure it scales properly */}
-        <Image source={require('./mascot-1.png')}/>
+        <Image source={require('../mascot-1.png')}/>
     </>
 
 }
