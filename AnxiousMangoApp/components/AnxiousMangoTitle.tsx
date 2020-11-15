8d48acca85fc5e2f787364f9ec69612a9e77a626
Monica Iqbal <monica.iqbal@mail.utoronto.ca>
2020-11-15 12:22:49 -0500

    added width and height to image

--- AnxiousMangoApp/components/AnxiousMangoTitle.tsx
+++ AnxiousMangoApp/components/AnxiousMangoTitle.tsx
@@ -4,7 +4,9 @@ export default function AnxiousMangoTitle()  {
     return <>
         <View style={styles.getStartedContainer}>
             Anxious Mango
-            <Image source={require('../mascot-1.png')}/>
+            <Image
+                style={{width: 100, height: 100}}
+                source={require('../mascot-1.png')}/>
         </View>
         {/* TODO add the anxious mango image here and make sure it scales properly */}
     </>
