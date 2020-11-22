d2d0773b8d0366b5f60aefb6fc2905c2dc575ad4
Monica Iqbal <monica.iqbal@mail.utoronto.ca>
2020-11-21 23:02:15 -0500

    added TableView to thought records section of page

--- AnxiousMangoApp/components/AnxiousMangoTitle.tsx
+++ AnxiousMangoApp/components/AnxiousMangoTitle.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
-import {Button, Image, StyleSheet, View} from "react-native";
+import {Button, Image, StyleSheet, View, ScrollView} from "react-native";
+import { Cell, Section, TableView } from 'react-native-tableview-simple';
+
+const tableViewStyles = {
+    backgroundColor: '#D39999',
+    paddingTop: 20,
+    paddingBottom: 20
+};
+
 export default function AnxiousMangoTitle()  {
     return <>
         <View style={styles.getStartedContainer}>
@@ -7,9 +15,21 @@ export default function AnxiousMangoTitle()  {
             <Image
                 style={{width: 100, height: 100}}
                 source={require('../mascot-1.png')}/>
-                Thought records:
+            <ThoughtRecords/>
             <CreateThoughtRecordButton/>
         </View>
+    </>
+}
+
+function ThoughtRecords() {
+    return <>
+        <ScrollView contentContainerStyle={tableViewStyles}>
+            <TableView>
+                <Section header="Thought Records" headerTextColor="#000000">
+                    <Cell cellStyle="Subtitle" backgroundColor="#EBAF83" title="Thought record goes here." detail="Dec 31, 1969 11:59PM"/>
+                </Section>
+            </TableView>
+        </ScrollView>
     </>
 }
 
@@ -18,7 +38,8 @@ function CreateThoughtRecordButton() {
         <Button
             title="Create New Thought Record"
             onPress={() => {
-                alert("Create button pressed")}
+                    alert("Create button pressed")
+                }
             }
             color="#D39999"/>
    </>
