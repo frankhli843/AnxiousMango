d62bcadf856037f17ffe701720ea6136b7ceb839
Monica Iqbal <monica.iqbal@mail.utoronto.ca>
2020-11-21 19:21:34 -0500

    added rest of dashboard elements

--- AnxiousMangoApp/components/AnxiousMangoTitle.tsx
+++ AnxiousMangoApp/components/AnxiousMangoTitle.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Image, StyleSheet, View} from "react-native";
+import {Button, Image, StyleSheet, View} from "react-native";
 export default function AnxiousMangoTitle()  {
     return <>
         <View style={styles.getStartedContainer}>
@@ -7,10 +7,21 @@ export default function AnxiousMangoTitle()  {
             <Image
                 style={{width: 100, height: 100}}
                 source={require('../mascot-1.png')}/>
+                Thought records:
+            <CreateThoughtRecordButton/>
         </View>
-        {/* TODO add the anxious mango image here and make sure it scales properly */}
     </>
+}
 
+function CreateThoughtRecordButton() {
+    return <>
+        <Button
+            title="Create New Thought Record"
+            onPress={() => {
+                alert("Create button pressed")}
+            }
+            color="#D39999"/>
+   </>
 }
 
 const styles = StyleSheet.create({
