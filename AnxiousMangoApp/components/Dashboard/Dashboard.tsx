1476f857e2239daf4ec3dc83fde2481012768cca
Monica Iqbal <monica.iqbal@mail.utoronto.ca>
2020-12-11 16:46:37 -0500

    imported Card from react-native-elements; thought record data boxes now visible

--- AnxiousMangoApp/components/Dashboard/Dashboard.tsx
+++ AnxiousMangoApp/components/Dashboard/Dashboard.tsx
@@ -1,18 +1,23 @@
 import * as React from 'react';
 import {Image, StyleSheet, View} from "react-native";
 import ThoughtRecords from "./ThoughtRecords";
-import NavStack from "./CreateThoughtRecordButton";
+import CreateThoughtRecordButton from "./MainTabNavButton";
 import {DashboardData, dashboardDemoData} from "../../types/DashboardData";
-import CreateThoughtRecordButton from "./CreateThoughtRecordButton";
 // install SyncStorage
 // import SyncStorage from 'sync-storage'
 
+type DashboardProps = {
+}
 
-export const Dashboard = () =>  {
+export const Dashboard = ({
+}: DashboardProps) =>  {
+    // TODO make it so that you can save a string and retrieve it once the app is refreshed
     // const dashboardData: DashboardData = JSON.parse(SyncStorage.get("CONST_Dashboard_DATA"))
     // SyncStorage.set("CONST_DASHBOARD_DATA", JSON.stringify(dashboardData));
 
+    // TODO use this data to show a thought record
     const dashboardData: DashboardData = dashboardDemoData;
+
     return <>
         <View style={styles.getStartedContainer}>
             Anxious Mango
@@ -20,7 +25,6 @@ export const Dashboard = () =>  {
                 style={{width: 100, height: 100, marginBottom: 5}}
                 source={require('../../mascot-1.png')}/>
             <ThoughtRecords/>
-            <NavStack/>
         </View>
     </>
 }
